3ee3b294f57df44909e50d66831a681bfa72670e
milanistie13 <milanistie13@gmail.com>
2024-12-29 11:08:07 +0700

    second commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,10 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 5,
+  TITLE: "Shopperqueries",
+  DESCRIPTION: "Elevate Your Shopping Experience with Shopperqueries. Explore a new realm of shopping satisfaction with Shopper Queries, your trusted guide to elevated shopping experiences.",
+  EMAIL: "solucionermedia@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 400,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
@@ -27,14 +27,14 @@ export const PROJECTS: Metadata = {
 export const SOCIALS: Socials = [
   {
     NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheeks",
+    HREF: "https://twitter.com/shopperqueries",
   },
   {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/shopperqueries",
   },
   {
     NAME: "LinkedIn",
-    HREF: "https://www.linkedin.com/in/trevortylerlee",
+    HREF: "https://www.linkedin.com/in/shopperqueries",
   },
 ];
